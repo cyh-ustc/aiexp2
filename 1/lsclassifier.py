23a77938f3de86da661ab735127270b5c0532d01
cyh <cyh88888@mail.ustc.edu.cn>
2018-07-02 08:51:42 +0800

    lsclassifier

--- 1/lsclassifier.py
+++ 1/lsclassifier.py
@@ -10,14 +10,42 @@ def lsClassifier(traindata, trainlabel, testdata, testlabel, _lambda):
 
 	omega = (z + _lambda*np.eye(z.shape[0])).I * x.T * y.T
 	predicty =  testdata * omega
-	print(predicty)
 	
 	
-
-
-
-
-
+	p = [i[0] for i in predicty]
+	p1 = p[:50]
+	p2 = p[50:]
+	p1.sort()
+	mf = 50
+	mt = 0
+	for i in range(len(p1)):
+		m2 = 0
+		for j in p2:
+			if j > p1[i]:
+				m2 += 1
+		f = (50 + m2)/(50 - i + 1)
+		if f < mf:
+			mf = f
+			mt = p1[i]
+	
+	np2p = 0
+	nn2p = 0
+	ypred = []
+	for i in range(len(p)):
+		if p[i] > mt:
+			ypred.append(1)
+			if testlabel[0, i]:
+				np2p += 1
+			else:
+				nn2p += 1
+		else:
+			ypred.append(0)
+	#print(nn2p, np2p)
+	SP = np2p / (np2p + nn2p) 
+	SR = np2p / 50
+	F = SP * SR * 2 / (SP + SR)
+	return ypred, SP, SR, F
+	
 
 sn, hn, M = tdm()
 
@@ -54,4 +82,4 @@ trainlabel = np.mat([1 for i in range(450)] + [0 for i in range(2250)])
 testlabel = np.mat([1 for i in range(50)] + [0 for i in range(250)])
 
 
-lsClassifier(traindata, trainlabel, testdata, testlabel, 0.1)
+print(lsClassifier(traindata, trainlabel, testdata, testlabel, 1))
--- 2/PCA.py
+++ 2/PCA.py
@@ -2,23 +2,28 @@
 import numpy as np
 import random
 
+# load images
 def loadpgm(path):
 	f = open(path, 'rb')
 	f.read(0xd)
 	return list(f.read(92 * 112))
 
-	
+
 def PCA(traindata, testdata, threshold):
+	# compute sigma
 	sigma = 0
 	sigma = traindata.T * traindata
 	sigma = sigma / 320
-	
+	# compute eigs
 	w, v = np.linalg.eig(sigma)
+	# ignore imag part
 	w = w.real
 	v = v.real
+	# sum of eigs
 	se = sum(w) * threshold
 	ze = 0
 	i = 0
+	# select first i eigs sum(v[:i]) >= sum(v) * threshold
 	while True:
 		ze += w[i]
 		i += 1
@@ -26,7 +31,7 @@ def PCA(traindata, testdata, threshold):
 			break
 	return v[:i]
 
-
+# predict
 def predict(traindata, testdata):
 	bingo = 0
 	for i in range(40):
@@ -34,10 +39,12 @@ def predict(traindata, testdata):
 			z = [np.sum(np.abs(x - testdata[i * 2 + j])) for x in traindata]
 			m = z[0]
 			midx = 0
+			# find the nearest face
 			for k in range(40 * 8):
 				if z[k] < m:
 					m = z[k]
 					midx = k
+			# predicted face index: midx // 8
 			if midx//8 == i:
 				bingo += 1
 	print(bingo/80)
@@ -54,7 +61,7 @@ for i in range(40):#40 persons
 	for j in range(2):
 		testdata.append(loadpgm('faces/s%d/%d.pgm'%(i+1, x[j+8] + 1)))
 
-
+# convert to numpy matrix
 traindata = np.mat(traindata)
 testdata = np.mat(testdata)
 
@@ -66,7 +73,7 @@ for i in range(len(testdata)):
 	testdata[i] = testdata[i] - mean_traindata
 
 
-# PCA
+# PCA eg. threshold = 0.9
 u = PCA(traindata, testdata, 0.9)
 
 # predict
